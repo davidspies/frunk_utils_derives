48c140ad2081c936b244d663e9c7def2112f99fb
David Spies <dnspies@gmail.com>
2025-04-27 09:50:36 -0700

    More where bounds

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, parse_quote, Data, DeriveInput, Fields, Index};
+use syn::{Data, DeriveInput, Fields, Index, parse_macro_input, parse_quote};
 
 #[proc_macro_derive(ToRef)]
 pub fn to_ref_derive(input: TokenStream) -> TokenStream {
@@ -12,15 +12,47 @@ pub fn to_ref_derive(input: TokenStream) -> TokenStream {
     let ref_struct_name = syn::Ident::new(&format!("{}FieldRefs", struct_name), struct_name.span());
     let struct_vis = &input.vis;
 
-    // Adjust generics to include a new lifetime 'z
-    let mut generics = input.generics.clone();
-    let lifetime_def: syn::LifetimeParam = parse_quote!('z);
-    generics
-        .params
-        .push(syn::GenericParam::Lifetime(lifetime_def));
+    // Original generics (for the struct type in the impl)
+    let generics_orig = &input.generics;
+    let (_, ty_generics_orig, _) = generics_orig.split_for_impl();
 
-    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
-    let (_, ty_generics_no_lifetime, _) = input.generics.split_for_impl();
+    // Generics with 'z added (for struct definition and impl <...>)
+    let mut generics_with_z = generics_orig.clone();
+    let lifetime_def: syn::LifetimeParam = parse_quote!('z);
+    generics_with_z
+        .params
+        .push(syn::GenericParam::Lifetime(lifetime_def.clone()));
+    let (impl_generics_with_z, ty_generics_with_z, where_clause_with_z_bounds) =
+        generics_with_z.split_for_impl();
+
+    // Where clause for the impl block (original bounds + FieldTy: 'z bounds)
+    let mut where_clause_for_impl =
+        where_clause_with_z_bounds
+            .cloned()
+            .unwrap_or_else(|| syn::WhereClause {
+                where_token: Default::default(),
+                predicates: Default::default(),
+            });
+
+    // Add field type bounds: FieldTy: 'z
+    let z_lifetime_ref = &lifetime_def.lifetime;
+    match &input.data {
+        Data::Struct(data) => {
+            let fields_iter = match &data.fields {
+                Fields::Named(fields) => fields.named.iter().map(|f| &f.ty).collect::<Vec<_>>(),
+                Fields::Unnamed(fields) => fields.unnamed.iter().map(|f| &f.ty).collect::<Vec<_>>(),
+                Fields::Unit => {
+                    panic!("ToRef derive macro does not support unit structs")
+                }
+            };
+            for field_ty in fields_iter {
+                where_clause_for_impl
+                    .predicates
+                    .push(parse_quote!(#field_ty: #z_lifetime_ref));
+            }
+        }
+        _ => panic!("ToRef derive macro only supports structs"),
+    };
 
     let output = match input.data {
         Data::Struct(data) => match data.fields {
@@ -29,20 +61,22 @@ pub fn to_ref_derive(input: TokenStream) -> TokenStream {
                     let name = &f.ident;
                     let ty = &f.ty;
                     let vis = &f.vis;
-                    quote! { #vis #name: &'z #ty }
+                    quote! { #vis #name: &'z #ty } // Use 'z here
                 });
                 let to_ref_body = fields.named.iter().map(|f| {
                     let name = &f.ident;
                     quote! { #name: &self.#name }
                 });
                 quote! {
+                    // Struct uses generics with 'z
                     #[derive(frunk::Generic, frunk::LabelledGeneric)]
-                    #struct_vis struct #ref_struct_name #impl_generics {
+                    #struct_vis struct #ref_struct_name #impl_generics_with_z #where_clause_with_z_bounds {
                         #( #field_defs, )*
                     }
 
-                    impl #impl_generics frunk::traits::ToRef<'z> for #struct_name #ty_generics_no_lifetime #where_clause {
-                        type Output = #ref_struct_name #ty_generics;
+                    // Impl uses generics with 'z for <...>, original generics for Struct<...>, extended where clause
+                    impl #impl_generics_with_z frunk::traits::ToRef<'z> for #struct_name #ty_generics_orig #where_clause_for_impl {
+                        type Output = #ref_struct_name #ty_generics_with_z;
 
                         fn to_ref(&'z self) -> Self::Output {
                             #ref_struct_name {
@@ -56,20 +90,22 @@ pub fn to_ref_derive(input: TokenStream) -> TokenStream {
                 let field_defs = fields.unnamed.iter().map(|f| {
                     let ty = &f.ty;
                     let vis = &f.vis;
-                    quote! { #vis &'z #ty }
+                    quote! { #vis &'z #ty } // Use 'z here
                 });
                 let to_ref_body = (0..fields.unnamed.len()).map(|i| {
                     let idx = Index::from(i);
                     quote! { &self.#idx }
                 });
                 quote! {
+                    // Struct uses generics with 'z
                     #[derive(frunk::Generic)]
-                    #struct_vis struct #ref_struct_name #impl_generics (
+                    #struct_vis struct #ref_struct_name #impl_generics_with_z #where_clause_with_z_bounds (
                         #( #field_defs, )*
                     );
 
-                    impl #impl_generics frunk::traits::ToRef<'z> for #struct_name #ty_generics_no_lifetime #where_clause {
-                        type Output = #ref_struct_name #ty_generics;
+                    // Impl uses generics with 'z for <...>, original generics for Struct<...>, extended where clause
+                    impl #impl_generics_with_z frunk::traits::ToRef<'z> for #struct_name #ty_generics_orig #where_clause_for_impl {
+                        type Output = #ref_struct_name #ty_generics_with_z; // Output uses generics with 'z
 
                         fn to_ref(&'z self) -> Self::Output {
                             #ref_struct_name (
@@ -96,15 +132,47 @@ pub fn to_mut_derive(input: TokenStream) -> TokenStream {
         syn::Ident::new(&format!("{}FieldMutRefs", struct_name), struct_name.span());
     let struct_vis = &input.vis;
 
-    // Adjust generics to include a new lifetime 'z
-    let mut generics = input.generics.clone();
-    let lifetime_def: syn::LifetimeParam = parse_quote!('z);
-    generics
-        .params
-        .push(syn::GenericParam::Lifetime(lifetime_def));
+    // Original generics (for the struct type in the impl)
+    let generics_orig = &input.generics;
+    let (_, ty_generics_orig, _) = generics_orig.split_for_impl();
 
-    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
-    let (_, ty_generics_no_lifetime, _) = input.generics.split_for_impl();
+    // Generics with 'z added (for struct definition and impl <...>)
+    let mut generics_with_z = generics_orig.clone();
+    let lifetime_def: syn::LifetimeParam = parse_quote!('z);
+    generics_with_z
+        .params
+        .push(syn::GenericParam::Lifetime(lifetime_def.clone()));
+    let (impl_generics_with_z, ty_generics_with_z, where_clause_with_z_bounds) =
+        generics_with_z.split_for_impl();
+
+    // Where clause for the impl block (original bounds + FieldTy: 'z bounds)
+    let mut where_clause_for_impl =
+        where_clause_with_z_bounds
+            .cloned()
+            .unwrap_or_else(|| syn::WhereClause {
+                where_token: Default::default(),
+                predicates: Default::default(),
+            });
+
+    // Add field type bounds: FieldTy: 'z
+    let z_lifetime_ref = &lifetime_def.lifetime;
+    match &input.data {
+        Data::Struct(data) => {
+            let fields_iter = match &data.fields {
+                Fields::Named(fields) => fields.named.iter().map(|f| &f.ty).collect::<Vec<_>>(),
+                Fields::Unnamed(fields) => fields.unnamed.iter().map(|f| &f.ty).collect::<Vec<_>>(),
+                Fields::Unit => {
+                    panic!("ToMut derive macro does not support unit structs")
+                }
+            };
+            for field_ty in fields_iter {
+                where_clause_for_impl
+                    .predicates
+                    .push(parse_quote!(#field_ty: #z_lifetime_ref));
+            }
+        }
+        _ => panic!("ToMut derive macro only supports structs"),
+    };
 
     let output = match input.data {
         Data::Struct(data) => match data.fields {
@@ -121,12 +189,12 @@ pub fn to_mut_derive(input: TokenStream) -> TokenStream {
                 });
                 quote! {
                     #[derive(frunk::Generic, frunk::LabelledGeneric)]
-                    #struct_vis struct #ref_struct_name #impl_generics {
+                    #struct_vis struct #ref_struct_name #impl_generics_with_z #where_clause_with_z_bounds {
                         #( #field_defs, )*
                     }
 
-                    impl #impl_generics frunk::traits::ToMut<'z> for #struct_name #ty_generics_no_lifetime #where_clause {
-                        type Output = #ref_struct_name #ty_generics;
+                    impl #impl_generics_with_z frunk::traits::ToMut<'z> for #struct_name #ty_generics_orig #where_clause_for_impl {
+                        type Output = #ref_struct_name #ty_generics_with_z;
 
                         fn to_mut(&'z mut self) -> Self::Output {
                             #ref_struct_name {
@@ -148,12 +216,12 @@ pub fn to_mut_derive(input: TokenStream) -> TokenStream {
                 });
                 quote! {
                     #[derive(frunk::Generic)]
-                    #struct_vis struct #ref_struct_name #impl_generics (
+                    #struct_vis struct #ref_struct_name #impl_generics_with_z #where_clause_with_z_bounds (
                         #( #field_defs, )*
                     );
 
-                    impl #impl_generics frunk::traits::ToMut<'z> for #struct_name #ty_generics_no_lifetime #where_clause {
-                        type Output = #ref_struct_name #ty_generics;
+                    impl #impl_generics_with_z frunk::traits::ToMut<'z> for #struct_name #ty_generics_orig #where_clause_for_impl {
+                        type Output = #ref_struct_name #ty_generics_with_z; // Output uses generics with 'z
 
                         fn to_mut(&'z mut self) -> Self::Output {
                             #ref_struct_name (
@@ -163,9 +231,9 @@ pub fn to_mut_derive(input: TokenStream) -> TokenStream {
                     }
                 }
             }
-            Fields::Unit => panic!("ToMut derive macro does not support unit structs"),
+            Fields::Unit => panic!("ToMut derive macro does not support unit structs"), // Already handled above
         },
-        _ => panic!("ToMut derive macro only supports structs"),
+        _ => panic!("ToMut derive macro only supports structs"), // Already handled above
     };
 
     output.into()
